41c9f35095f021b9e7d5bb5461bbe87aa45962b2
Sophie Dufrane <dufranesophie@gmail.com>
2024-09-22 17:07:44 +0000

    Add function to send the stock data to the user in a form of dictionary

--- run.py
+++ run.py
@@ -117,6 +117,20 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Retrieve headings of the stock worksheet.
+    Create a dictionary mapping headings and data values.
+    """
+    print("Retrieving stock values data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+
+    headings = stock[0]
+
+    stock_dict = {heading: value for heading, value in zip(headings, data)}
+
+    print(f"Make the following numbers of sandwiches for next market:\n")
+    return stock_dict
 
 def main():
     """
@@ -130,7 +144,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
